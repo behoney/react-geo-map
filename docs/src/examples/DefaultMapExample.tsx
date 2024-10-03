bb2c8cba98b7453bdc56f1ffacea850146617f93
Byounghern Kim <be.honey@si-analytics.ai>
2024-10-03 21:20:07 +0900

    refactor: rename files

--- docs/src/examples/DefaultMapExample.tsx
+++ docs/src/examples/DefaultMapExample.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { GeoDataSource, GeoMap } from '../../../src/lib';
 
-const DataSource: React.FC = () => {
+const DefaultMapExample: React.FC = () => {
   return (
     <div style={{ width: '300px', height: '300px' }}>
       <GeoMap className='w-full h-full'>
-        <GeoDataSource url="sample.geojson" />
+        <GeoDataSource url="https://openlayers.org/data/geojson/countries.geojson" />
       </GeoMap>
     </div>
   );
 };
 
-export default DataSource;
+export default DefaultMapExample;
