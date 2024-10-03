6fb35beea9c340493cff395149217b6aa3d4493b
Byounghern Kim <be.honey@si-analytics.ai>
2024-10-03 20:38:07 +0900

    chore: clean up things

--- tests/snapshot.spec.ts
+++ tests/snapshot.spec.ts
@@ -7,15 +7,9 @@ test(
 
 		await page.goto("http://localhost:4321/test");
 
-		console.log("Page loaded");
-
-		console.log(await page.content());
-
 		await page.waitForSelector(".ol-viewport", { timeout: 30000 });
 
 		await page.waitForTimeout(1000);
-
-		console.log("Map viewport found");
 
 		expect(await page.screenshot()).toMatchSnapshot("map.png");
 	},
