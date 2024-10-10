e567ff51c8b781baa4e385a193f0f4a5a3898555
Byounghern Kim <be.honey@si-analytics.ai>
2024-10-10 21:06:14 +0900

    docs: update example

--- docs/src/examples/EventHandlerExample.tsx
+++ docs/src/examples/EventHandlerExample.tsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { GeoDataSource, GeoMap } from "../../../src/lib";
 
 const EventHandlerExample: React.FC = () => {
+  const [clickedFeature, setClickedFeature] = useState("");
+  const [hoveredFeature, setHoveredFeature] = useState("");
   return (
-    <div style={{ width: "300px", height: "300px" }}>
-      <GeoMap>
-        <GeoDataSource
-          fitViewToData={true}
-          url={`${import.meta.env.BASE_URL}sample.geojson`}
-          onClick={(event) => {
-            console.log("onClick", event);
-          }}
-          onMissed={() => {
-            console.log("onMissed");
-          }}
-          onHover={(event) => {
-            console.log("onHover", event);
-          }}
-          onDoubleClick={(event) => {
-            console.log("onDoubleClick", event);
-          }}
-        />
-      </GeoMap>
-    </div>
+    <section>
+      <h1>EventHandlerExample</h1>
+      <div style={{ width: "300px", height: "300px" }}>
+        <GeoMap>
+          <GeoDataSource
+            fitViewToData={true}
+            url={`${import.meta.env.BASE_URL}sample.geojson`}
+            onClick={(feature) => {
+              setClickedFeature(feature.name);
+            }}
+            onMissed={() => {
+              setHoveredFeature("");
+            }}
+            onHover={(feature) => {
+              setHoveredFeature(feature.name);
+            }}
+          />
+        </GeoMap>
+      </div>
+      <ul>
+        <li>Clicked Feature: {clickedFeature}</li>
+        <li>Hovered Feature: {hoveredFeature}</li>
+      </ul>
+    </section>
   );
 };
 
