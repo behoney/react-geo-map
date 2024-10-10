dfd4fb1a1ed6fd1b76beea351045e744af706132
Byounghern Kim <be.honey@si-analytics.ai>
2024-10-10 20:08:30 +0900

    feat: event handlers

--- docs/src/examples/EventHandlerExample.tsx
+++ docs/src/examples/EventHandlerExample.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { GeoDataSource, GeoMap } from "../../../src/lib";
 
-const HelloWorldExample: React.FC = () => {
+const EventHandlerExample: React.FC = () => {
   return (
     <div style={{ width: "300px", height: "300px" }}>
-      <GeoMap>
+      <GeoMap
+        onClick={(event) => {
+          console.log("onClick", event);
+        }}
+      >
         <GeoDataSource
           fitViewToData={true}
-          url={`${import.meta.env.BASE_URL}hello-world.geojson`}
+          url={`${import.meta.env.BASE_URL}sample.geojson`}
         />
       </GeoMap>
     </div>
   );
 };
 
-export default HelloWorldExample;
+export default EventHandlerExample;
